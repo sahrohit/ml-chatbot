f9300a02734997d07df3c389f9cf88c092b591e0
sahrohit <sahrohit9586@hmail.com>
2021-10-24 02:02:40 +0545

    Added NProgress Bar

--- frontend/pages/_app.js
+++ frontend/pages/_app.js
@@ -2,6 +2,14 @@ import "../styles/globals.css";
 import { ChakraProvider } from "@chakra-ui/react";
 import { AnimatePresence } from "framer-motion";
 
+import Router from "next/router";
+import NProgress from "nprogress";
+import "nprogress/nprogress.css";
+
+Router.events.on("routeChangeStart", () => NProgress.start());
+Router.events.on("routeChangeComplete", () => NProgress.done());
+Router.events.on("routeChangeError", () => NProgress.done());
+
 function MyApp({ Component, pageProps }) {
 	return (
 		<AnimatePresence exitBeforeEnter>
