2cbf9f9fbe64b2c5f3d4b77ab78aa80cc6ff775a
sahrohit <sahrohit9586@hmail.com>
2021-12-11 19:39:15 +0545

    Added Google Analytics

--- frontend/pages/_app.js
+++ frontend/pages/_app.js
@@ -1,8 +1,11 @@
 import "../styles/globals.css";
 import { ChakraProvider } from "@chakra-ui/react";
 import { AnimatePresence } from "framer-motion";
+import Script from "next/script";
+import * as gtag from "../lib/gtag";
+import { useEffect } from "react";
 
-import Router from "next/router";
+import Router, { useRouter } from "next/router";
 import NProgress from "nprogress";
 import "nprogress/nprogress.css";
 
@@ -11,12 +14,43 @@ Router.events.on("routeChangeComplete", () => NProgress.done());
 Router.events.on("routeChangeError", () => NProgress.done());
 
 function MyApp({ Component, pageProps }) {
+	const router = useRouter();
+	useEffect(() => {
+		const handleRouteChange = (url) => {
+			gtag.pageview(url);
+		};
+		router.events.on("routeChangeComplete", handleRouteChange);
+		return () => {
+			router.events.off("routeChangeComplete", handleRouteChange);
+		};
+	}, [router.events]);
+
 	return (
-		<AnimatePresence exitBeforeEnter>
-			<ChakraProvider>
-				<Component {...pageProps} />
-			</ChakraProvider>
-		</AnimatePresence>
+		<>
+			<Script
+				strategy="afterInteractive"
+				src={`https://www.googletagmanager.com/gtag/js?id=${process.env.NEXT_PUBLIC_GA_ID}`}
+			/>
+			<Script
+				id="gtag-init"
+				strategy="afterInteractive"
+				dangerouslySetInnerHTML={{
+					__html: `
+            window.dataLayer = window.dataLayer || [];
+            function gtag(){dataLayer.push(arguments);}
+            gtag('js', new Date());
+            gtag('config', '${process.env.NEXT_PUBLIC_GA_ID}', {
+              page_path: window.location.pathname,
+            });
+          `,
+				}}
+			/>
+			<AnimatePresence exitBeforeEnter>
+				<ChakraProvider>
+					<Component {...pageProps} />
+				</ChakraProvider>
+			</AnimatePresence>
+		</>
 	);
 }
 
