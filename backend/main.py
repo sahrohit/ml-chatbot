110aa080e758fc797fa4a9c5725204565016bd4d
Rohit Sah <sahrohit9586@gmail.com>
2022-10-31 21:30:47 +0530

    too bored didn't read

--- backend/main.py
+++ backend/main.py
@@ -21,7 +21,7 @@ def ping():
     return "Pinging Model Application"
 
 
-@app.route("/api/chat", methods=["POST"])
+@app.route("/api/generate", methods=["POST"])
 def api():
 
     input_json = request.json
@@ -106,52 +106,6 @@ def api():
     except:
         print("Couldn't Write to the Database")
 
-    return jsonify(response)
-
-
-@app.route("/chat/<inputquestion>", methods=["GET"])
-def chatnow(inputquestion):
-
-    device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
-
-    with open("intents.json", "r") as f:
-        intents = json.load(f)
-
-    FILE = "data.pth"
-    data = torch.load(FILE)
-
-    input_size = data["input_size"]
-    hidden_size = data["hidden_size"]
-    output_size = data["output_size"]
-    all_words = data["all_words"]
-    tags = data["tags"]
-    model_state = data["model_state"]
-
-    model = NeuralNet(input_size, hidden_size, output_size).to(device)
-    model.load_state_dict(model_state)
-    model.eval()
-
-    sentence = inputquestion
-    sentence = tokenize(sentence)
-    X = bag_of_words(sentence, all_words)
-    X = X.reshape(1, X.shape[0])
-    X = torch.from_numpy(X).to(device)
-
-    output = model(X)
-    _, predicted = torch.max(output, dim=1)
-    tag = tags[predicted.item()]
-
-    probs = torch.softmax(output, dim=1)
-    prob = probs[0][predicted.item()]
-
-    if prob.item() > 0.50:
-        for intent in intents["intents"]:
-            if tag == intent["tag"]:
-                result = random.choice(intent["responses"])
-    else:
-        result = "I have no idea..."
-
-    response = {"answer": result}
     return jsonify(response)
 
 
--- backend/nltk_utils.py
+++ backend/nltk_utils.py
@@ -22,3 +22,28 @@ def bag_of_words(tokenized_sentence, all_words):
         if w in sentence_words:
             bag[idx] = 1.0
     return bag
+
+def Lyrics_Generator(starter,Ch_count): #,temperature=1.0):
+    generated= ""
+    starter = starter 
+    seed=[mapping[char] for char in starter]
+    generated += starter 
+    # Generating new text of given length
+    for i in range(Ch_count):
+        seed=[mapping[char] for char in starter]
+        x_pred = np.reshape(seed, (1, len(seed), 1))
+        x_pred = x_pred/ float(L_symb)
+        prediction = model.predict(x_pred, verbose=0)[0]  
+        # Getting the index of the next most probable index
+        prediction = np.asarray(prediction).astype('float64')
+        prediction = np.log(prediction) / 1.0 
+        exp_preds = np.exp(prediction)
+        prediction = exp_preds / np.sum(exp_preds)
+        probas = np.random.multinomial(1, prediction, 1)
+        index = np.argmax(prediction)
+        next_char = reverse_mapping[index]  
+        # Generating new text
+        generated += next_char
+        starter = starter[1:] + next_char
+       
+    return generated
